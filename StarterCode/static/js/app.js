9e2445d3e3d762461e774c7c2c429be805f779e7
vijitdesilva <vijit.desilva@gmail.com>
2021-05-06 15:04:55 -0400

    updated

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,26 +1,32 @@
 // 1. Use the D3 library to read in samples.json.
-
-d3.json("data/samples.json").then(sampledata) => {
-// console.log(sampledata)
+function createPlots(id) {
+d3.json("samples.json").then(sampledata => {
+ console.log(sampledata)
+ var ids = sampledata.samples[0].otu_ids;
+ console.log(ids)
+ var sampleValues = sampledata.samples[0].sample_values;
+ console.log(sampleValues)
+ var otuLabels = sampledata.samples[0].otu_labels;
+ console.log(otuLabels)
 
 // create variable 
-    var otuData = sampledata;
+// var otuData = sampledata;
 
 // Sort the data array using the greekSearchResults value
-     otuData.sort(function(a, b) {
-        return parseFloat(b.sample_values) - parseFloat(a.sample_values);
+sampleValues.sort(function(a, b) {
+        return parseFloat(b.sampledata.samples.sample_values) - parseFloat(a.sampledata.samples.sample_values);
     });
 
 // Slice the first 10 objects for plotting
-    otuData = otuData.slice(0, 10);
+sampleValues = sampleValues.slice(0, 10);
 
 // Reverse the array due to Plotly's defaults
-    otuData = otuData.reverse();
+sampleValues = sampleValues.reverse();
 
 // create traces
     var trace1 = {
-        x: otuData.map(row => row.otu_ids),
-        y: otuData.map(row => row.sample_values),
+        x: ids,
+        y: sampleValues,
         type: "bar",
         name: "otu_labels",
         orientation: "h"
@@ -31,7 +37,7 @@ d3.json("data/samples.json").then(sampledata) => {
  // Define the plot layout
     var layout = {
         title: "Top 10 Bacteria Cultures Found",
-        yaxis:{
+        yaxis: {
         tickmode:"linear",
         },
         margin: {
