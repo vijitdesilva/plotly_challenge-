9465e0e413077eb7e53384fa16f4a04352043988
vijitdesilva <vijit.desilva@gmail.com>
2021-05-05 22:51:04 -0400

    bar

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,31 @@
-d3.json("data.samples.json").then(samples) => {
-    console.log(sampledata)
-    // create traces
+d3.json("data/samples.json").then(sampledata) => {
+// console.log(sampledata)
+
+// create variable 
+    var otuData = sampledata;
+
+// Sort the data array using the greekSearchResults value
+     otuData.sort(function(a, b) {
+    return parseFloat(b.samples.sample_values) - parseFloat(a.samples.sample_values);
+  });
+
+// Slice the first 10 objects for plotting
+    otuData = otuData.slice(0, 10);
+
+// Reverse the array due to Plotly's defaults
+    otuData = otuData.reverse();
+
+// create traces
     var trace1 = {
-        x: samples.otu_ids,
-        y: samples.sample_values,
+        x: otuData.map(row => row.otu_ids),
+        y: otuData.map(row => row.sample_values),
+        text: 
         type: "bar",
         name: "otu_labels",
-    }
+        orientation: "h"
 };
 // Create the data array for the plot
-var data = [trace1];
+    var chartData = [trace1];
 
  // Define the plot layout
 var layout = {
@@ -18,7 +34,7 @@ var layout = {
     yaxis: { title: "Square Root of Survival" }
   };
 
-    // Plot the chart to a div tag with id "plot"
-    Plotly.newPlot("plot", data, layout);
+// Plot the chart to a div tag with id "plot"
+Plotly.newPlot("bar", chartData, layout);
 });
 
