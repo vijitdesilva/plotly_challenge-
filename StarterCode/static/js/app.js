2e23634b0a4770ad8d089342dcea3f906a60f1b1
vijitdesilva <vijit.desilva@gmail.com>
2021-05-06 00:08:43 -0400

    update1

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,5 @@
+// 1. Use the D3 library to read in samples.json.
+
 d3.json("data/samples.json").then(sampledata) => {
 // console.log(sampledata)
 
@@ -6,8 +8,8 @@ d3.json("data/samples.json").then(sampledata) => {
 
 // Sort the data array using the greekSearchResults value
      otuData.sort(function(a, b) {
-    return parseFloat(b.samples.sample_values) - parseFloat(a.samples.sample_values);
-  });
+        return parseFloat(b.sample_values) - parseFloat(a.sample_values);
+    });
 
 // Slice the first 10 objects for plotting
     otuData = otuData.slice(0, 10);
@@ -19,22 +21,29 @@ d3.json("data/samples.json").then(sampledata) => {
     var trace1 = {
         x: otuData.map(row => row.otu_ids),
         y: otuData.map(row => row.sample_values),
-        text: 
         type: "bar",
         name: "otu_labels",
         orientation: "h"
-};
+    };
 // Create the data array for the plot
     var chartData = [trace1];
 
  // Define the plot layout
-var layout = {
-    title: "Top 10 OTUs",
-    xaxis: { title: "Organ" },
-    yaxis: { title: "Square Root of Survival" }
-  };
+    var layout = {
+        title: "Top 10 Bacteria Cultures Found",
+        yaxis:{
+        tickmode:"linear",
+        },
+        margin: {
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 30
+        }
+    };
 
-// Plot the chart to a div tag with id "plot"
-Plotly.newPlot("bar", chartData, layout);
+// Plot the chart to a div tag with id "bar"
+    Plotly.newPlot("bar", chartData, layout);
 });
 
+// 3. Create a bubble chart that displays each sample.
