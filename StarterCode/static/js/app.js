bd6aba117f6b621661a294ebc6a60ff278f1a06b
vijitdesilva <vijit.desilva@gmail.com>
2021-05-06 23:25:54 -0400

    updated

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,35 +1,37 @@
 // 1. Use the D3 library to read in samples.json.
-function createPlots(id) {
+// function createPlots(id) {
 d3.json("samples.json").then(sampledata => {
  console.log(sampledata)
  var ids = sampledata.samples[0].otu_ids;
  console.log(ids)
- var sampleValues = sampledata.samples[0].sample_values;
+ var sampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
  console.log(sampleValues)
- var otuLabels = sampledata.samples[0].otu_labels;
+ var otuLabels = sampledata.samples[0].otu_labels.slice(0,10);
  console.log(otuLabels)
 
 // create variable 
 // var otuData = sampledata;
 
-// Sort the data array using the greekSearchResults value
-sampleValues.sort(function(a, b) {
-        return parseFloat(b.sampledata.samples.sample_values) - parseFloat(a.sampledata.samples.sample_values);
-    });
+// sampleValues.sort(function(a, b) {
+//         return parseFloat(b.sampledata.samples.sample_values) - parseFloat(a.sampledata.samples.sample_values);
+//     });
 
-// Slice the first 10 objects for plotting
-sampleValues = sampleValues.slice(0, 10);
+var topOTU = (sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+var idOTU = topOTU.map(d => "OTU" + d);
+console.log(`OTU IDS: ${OTU_id}`)
+// first 10 objects for plotting
+var otuLabels = sampledata.samples[0].otu_labels.slice(0,10);
+console.log(otuLabels: ${OTU_id})
 
-// Reverse the array due to Plotly's defaults
-sampleValues = sampleValues.reverse();
 
 // create traces
     var trace1 = {
-        x: ids,
-        y: sampleValues,
+        x: sampleValues,
+        y: OTU_id,
+        text: otuLabels,
         type: "bar",
         name: "otu_labels",
-        orientation: "h"
+        orientation: "h",
     };
 // Create the data array for the plot
     var chartData = [trace1];
@@ -52,29 +54,29 @@ sampleValues = sampleValues.reverse();
     Plotly.newPlot("bar", chartData, layout);
 });
 
-// 3. Create a bubble chart that displays each sample.
-var trace2 = {
-    x: sampledata.samples[0].otu_ids,
-    y: sampledata.samples[0].sample_values,
-    mode: "markers",
-    marker: {
-        size: sampledata.samples[0].sample_values,
-        color: sampledata.samples[0].otu_ids
-    },
-    text:  sampledata.samples[0].otu_labels
+// // 3. Create a bubble chart that displays each sample.
+// var trace2 = {
+//     x: sampledata.samples[0].otu_ids,
+//     y: sampledata.samples[0].sample_values,
+//     mode: "markers",
+//     marker: {
+//         size: sampledata.samples[0].sample_values,
+//         color: sampledata.samples[0].otu_ids
+//     },
+//     text:  sampledata.samples[0].otu_labels
 
-};
+// };
 
-// set the layout for the bubble plot
-var layout2 = {
-    xaxis:{title: "OTU ID"},
-    height: 600,
-    width: 1000
-};
+// // set the layout for the bubble plot
+// var layout2 = {
+//     xaxis:{title: "OTU ID"},
+//     height: 600,
+//     width: 1000
+// };
 
-// creating data variable 
-var data2 = [trace2];
+// // creating data variable 
+// var data2 = [trace2];
 
-// create the bubble plot
-Plotly.newPlot("bubble", data2, layout2); 
+// // create the bubble plot
+// Plotly.newPlot("bubble", data2, layout2); 
 
