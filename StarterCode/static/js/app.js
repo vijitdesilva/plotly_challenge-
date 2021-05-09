f58dda224dd90d30c64f9707e83a0060e89201e6
vijitdesilva <vijit.desilva@gmail.com>
2021-05-08 23:39:08 -0400

    added

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -113,16 +113,16 @@ function createPlots(id) {
         // dropdown menu 
         var dropdown = d3.select("#selDataset");
     
-        // read the data 
+        // read json
         d3.json("samples.json").then((data)=> {
             console.log(data)
     
-            // get the id data to the dropdwown menu
+            // id data to the dropdwown menu
             data.names.forEach(function(name) {
                 dropdown.append("option").text(name).property("value");
             });
     
-            // call the functions to display the data and the plots to the page
+            // call the functions 
             createPlots(data.names[0]);
             getDemo(data.names[0]);
         });
